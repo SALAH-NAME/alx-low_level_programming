89ce1516564680c76a05a3177a0bfbc338fe81f9
SALAH-NAME <phone4ouahidi@gmail.com>
2023-02-16 11:04:08 +0000

    0x00-hello_world(fix1)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(){
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
