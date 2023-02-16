c7763620221c795f62562601acbccb5ddc4bed1f
SALAH-NAME <phone4ouahidi@gmail.com>
2023-02-16 11:11:25 +0000

    0x00-hello_world(fix2)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: prints "Programming is like building a multilingual puzzle"
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- *       
  */
 
-int main() {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
  */
 
-int main(){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
