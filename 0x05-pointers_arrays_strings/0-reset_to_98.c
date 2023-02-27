f77c8e05e2967f5a762522d6496d172f11f6289c
SALAH-NAME <phone4ouahidi@gmail.com>
2023-02-27 19:20:23 +0000

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - main
+ * reset_to_98 - reset to 98
+ */
+
+int reset_to_98(int* p)
+{
+*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int
+ * @a: integer swap
+ * @b: integer swap
+ */
+
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
