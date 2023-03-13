92fc1065465051d65f38f3a052ecf011d3fe608c
SALAH-NAME <phone4ouahidi@gmail.com>
2023-03-13 19:25:00 +0000

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars
+ * create_array - Creates an array of chars and
+ *             initializes it with a specific char.
  * @size: size of array
  * @c: char
  * Retrun: pointer to the array
