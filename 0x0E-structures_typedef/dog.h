2841fe07279794b7434113f3ea7b6525b24c3b2c
SALAH-NAME <phone4ouahidi@gmail.com>
2023-03-20 15:39:16 +0000

    prepro & struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,24 @@
 #define _DOG_H_
 
 /**
- * dog_t - typedef for struct dog
- */
-
-typedef struct god dog_t;
-
-/**
  * struct dog - struct that stores some information of a god
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
 
-struct god
+struct dog
 {
 	char *name;
 	float age;
 	char *ower;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
