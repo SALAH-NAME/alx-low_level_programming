784a974285681fa490bcbfbbafad0bdae18b4ed6
SALAH-NAME <phone4ouahidi@gmail.com>
2023-03-23 15:00:56 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,17 +1,17 @@
-#indlude "variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
- * print_numbers - prints number
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- * Return: nothing
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
+ * Return: no return.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
-	
+
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
