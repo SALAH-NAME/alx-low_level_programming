747c2cc82f2b8c2379fea2ad0e08bea1ad4c6858
SALAH-NAME <phone4ouahidi@gmail.com>
2023-04-10 21:22:18 +0000

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and print
+ * read_textfile - read a text file and prints letters
  * @filename: filename
  * @letters: numbers of letters printed
- * Return: numbers of letters printed, 0 if fails
+ * Return: numbers of letters printed, returns 0 if fials
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int f;
 	ssize_t num_read, num_write;
-	char *buffer;
+	char *buff;
 
 	if (!filename)
 		return (0);
 
 	f = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (f == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-
-	if (!buffer)
+	buff = malloc(sizeof(char) * (letters));
+	
+	if (!buff)
 		return (0);
 
-	num_read = read(f, buffer, letters);
-	num_write = write(STDOUT_FILEND, buffer, num_read);
+	num_read = read(f, buff, letters);
+	num_write = write(STDOUT_FILENO, buff, num_read);
 
 	close(f);
 
-	free(buffer);
+	free(buff);
 
 	return (num_write);
 }
