17e53c1e3abc8525b76c89f350cac31720a06343
SALAH-NAME <phone4ouahidi@gmail.com>
2023-04-10 18:20:20 +0000

    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * set_bit - set value of a bit to 1 at an index
- * @n: pointer of unsinged long int
+ * set_bit - set value of a bit to 1 an index
+ * @n: pointer of unsigned long int
  * @index: index of the bit
- * Return: 1 if it worked, -1 if any error
+ * Return: 1 if it worked, -1 if not
  */
 
-int set_bit(unsigned long *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int a;
-	
+
 	if (index > 63)
 		return (-1);
 
