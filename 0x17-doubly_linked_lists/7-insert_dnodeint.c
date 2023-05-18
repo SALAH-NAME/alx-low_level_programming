6fd1b1a6a3905d27c60a51cf7c5c2a68655c6ae8
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-18 23:29:32 +0000

    new files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 	dlistint_t *current = *h;
 	unsigned int i = 0;
 
@@ -17,13 +17,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (current && i < (idx))
+	while (current && i < (idx - 1))
 	{
 		current = current->next;
 		i++;
 	}
 	if (!current)
 		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 
