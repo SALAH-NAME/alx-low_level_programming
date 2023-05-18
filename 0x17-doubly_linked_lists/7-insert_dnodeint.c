74c0eaee736c6c10049e0e3d0b4db1383997ac82
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-18 23:04:58 +0000

    fix 7 try

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (current && i < (idx - 1))
+	while (current && i < (idx))
 	{
 		current = current->next;
 		i++;
