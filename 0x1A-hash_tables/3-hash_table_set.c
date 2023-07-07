4a386adfeb7f7b40aa910afdff86f20a211be2d8
SALAH-NAME <phone4ouahidi@gmail.com>
2023-07-07 18:04:55 +0000

    fix 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while (temp != NULL)
+	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
@@ -33,6 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 
+	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
